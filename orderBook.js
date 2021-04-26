b2183f117981f6a8abba490ca5a37167220456db
Michael <mlarock711@comcast.net>
2021-04-25 22:48:26 -0400

    Going to use new arrays for neworders.

--- orderBook.js
+++ orderBook.js
@@ -1,11 +1,13 @@
-const reconcileOrder = (existingBook, incomingOrder) => {
+function reconcileOrder(existingBook, incomingOrder) {
+  let updatedBook = []
+  let newOrders = []
+
+  // want to add to new arrays and check if book is empty 
   if (existingBook.length === 0) {
-    const updatedBook = existingBook.concat({ ...incomingOrder })
+    const updatedBook = existingBook.concat(incomingOrder)
 
     return updatedBook
   }
 }
 
 module.exports = reconcileOrder
-
-
